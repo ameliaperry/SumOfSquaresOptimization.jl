a4c0daa02ca8e4f754c98dc13b234bc8492dead9
Will Perry <will@willperry.me>
2015-12-19 22:22:42 -0500

    monom/poly unit tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,13 +3,39 @@ using Base.Test
 
 objtol = 1e-4
 
+function equals_upto_perm(a,b)
+    if length(a) != length(b)
+        return false
+    end
+    mapreduce(p -> b[p] == a, Base.OrFun(), permutations(1:length(b)))
+end
+@test equals_upto_perm([1,2],[2,1])
+
 # UNIT TESTS
 
-# TODO
+# monomials & polynomials
+a = Dict( :x1 => 3, :x2 => 2 ) 
+b = Dict( :x1 => 1, :x3 => 5 )
+c = Dict( :x1 => 4, :x2 => 2, :x3 => 5 )
+@test a * b == c
+@test SumOfSquaresOptimization.deg(a) == 5
+p = Dict(a=>1.0, b=>2.0)
+@test Dict(a=>1.0) + Dict(b=>2.0) == p
+@test SumOfSquaresOptimization.deg(p) == 6
+@test equals_upto_perm(SumOfSquaresOptimization.monoms([:x1,:x2],2), [ Dict(:x1=>2), Dict(:x1=>1, :x2=>1), Dict(:x2=>2) ])
+@test SumOfSquaresOptimization.monoms(0) == [SumOfSquaresOptimization.SoSMonom()]
+@test SumOfSquaresOptimization.monoms(3) == Array{SumOfSquaresOptimization.SoSMonom,1}()
+@test equals_upto_perm(SumOfSquaresOptimization.decomp(Dict(:x1=>4),3), [ (Dict(:x1=>3),Dict(:x1=>1)), (Dict(:x1=>2),Dict(:x1=>2)), (Dict(:x1=>1),Dict(:x1=>3)) ])
+
+# parsing
+@test SumOfSquaresOptimization.parsepoly(Program(), parse("x1^3 * x2^2")) == Dict(a => 1.0)
+@test SumOfSquaresOptimization.parsepoly(Program(), parse("x1^3 * x2^2 + 2.0 * x1 * x3^5")) == p
+
+
 
 # END-TO-END TESTS
 
-# unconstrained minimization
+# unconstrained minimization: a quadratic bowl
 prog = Program(minimize=true)
 objective(prog, "x^2 + y^2 - 1")
 sol = sossolve(prog,2)
@@ -33,6 +59,11 @@ for i in 1:6
     @partconstraint(prog, :bisect, "x%d", i)
 end
 @partconstraint(prog, :bisect, "-%d", 6/2)
+
+sol = sossolve(prog,2)
+@test abs(primalobj(sol) - 1.5) < objtol
+
+# test dihedral symmetry on the same program
 symmetrize_dihedral!(prog, [symbol("x$i") for i in 1:6])
 sol = sossolve(prog,2)
 @test abs(primalobj(sol) - 1.5) < objtol
